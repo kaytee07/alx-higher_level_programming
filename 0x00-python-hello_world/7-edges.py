bc22d5d0c22ee281b472f25db80dbafba439eaba
Winful KOfi Taylor <kofi.taylor07@gmail.com>
2023-03-07 17:55:30 +0000

    copy cut and paste

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -2,7 +2,7 @@
 word = "Holberton"
 # YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
 word_first_3 = word[:3]
-word_last_2 = word[7:]
+word_last_2 = word[-2:]
 middle_word = word[1:-1]
 print(f"First 3 letters: {word_first_3}")
 print(f"Last 2 letters: {word_last_2}")
