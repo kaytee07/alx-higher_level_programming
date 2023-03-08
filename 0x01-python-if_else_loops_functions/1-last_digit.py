5d23792ae8507375884d9f6ab250336bd3c9dc49
Winful KOfi Taylor <kofi.taylor07@gmail.com>
2023-03-08 11:08:52 +0000

    print the alphabet without importing modules

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
 import random
 
+
 def lastdigit(num):
-    last_digit_unsigned = abs(num)%10
+    last_digit_unsigned = abs(num) % 10
     if (num < 0):
         return -last_digit_unsigned
     else:
         return last_digit_unsigned
 
-number = random.randint(-10000, 10000)
-if lastdigit(number) == 0:
-    print("Last digit of {} is {} and is 0".format(number,lastdigit(number)))
-elif lastdigit(number) > 5:
-    print("Last digit of {} is {} and is greater than 5".format(number,lastdigit(number)))
-elif lastdigit(number) < 6 and number != 0:
-    print("Last digit of {} is {} and is less than 6 and not 0".format(number,lastdigit(number)))
 
+number = random.randint(-10000, 10000)
+lastdig = lastdigit(number)
+const = "Last digit of {} is {} and is".format(number, lastdig)
+if lastdig == 0:
+    print("{} 0".format(const))
+elif lastdig > 5:
+    print("{} greater than 5".format(const))
+elif lastdig < 6 and number != 0:
+    print("{} less than 6 and not 0".format(const))
