fd6364e6b8323f7b4b848fbe59a44cece0004e9d
Winful KOfi Taylor <kofi.taylor07@gmail.com>
2023-03-08 21:33:30 +0000

    pain

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -10,31 +10,21 @@
 
 listint_t *insert_node(listint_t **head, int number)
 {
-listint_t *new_node;
-listint_t *sorted_list = *head;
-new_node = malloc(sizeof(listint_t));
-if (new_node == NULL)
-return (NULL);
-new_node->n = number;
-new_node->next = NULL;
+	listint_t *node = *head, *new;
 
-if (sorted_list == NULL || sorted_list->n >= number)
-{
-new_node->next = sorted_list;
-*head = sorted_list;
-return (new_node);
-}
-
-while (sorted_list->next != NULL)
-{
-if (sorted_list && sorted_list->n < number &&
-(sorted_list->next->n > number || sorted_list->next == NULL))
-{
-new_node->next = sorted_list->next;
-sorted_list->next = new_node;
-break;
-}
-sorted_list = sorted_list->next;
-}
-return (sorted_list);
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = number;
+	if (node == NULL || node->n >= number)
+	{
+		new->next = node;
+		*head = new;
+		return (new);
+	}
+	while (node && node->next && node->next->n < number)
+		node = node->next;
+	new->next = node->next;
+	node->next = new;
+	return (new);
 }
