6885e7bec17862caad61c3931b6a2993998d3bb8
Winful KOfi Taylor <kofi.taylor07@gmail.com>
2023-03-08 18:14:54 +0000

    errors

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for i in range(122, 64, -1):
-    char = chr(i)
+for i in range(ord('z'), ord('a') - 1, -1):
     if i % 2 == 0:
-        print(char.upper(), end='')
+        diff = 0
     else:
-        print(char.lower(), end='')
+        diff = 32
+    print('{}'.format(chr(i - diff)), end='')
