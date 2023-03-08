d70fe206090ed22f916937a223e9d9f7f11634f6
Winful KOfi Taylor <kofi.taylor07@gmail.com>
2023-03-08 17:45:56 +0000

    remove p and q

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 for letter in range(97, 123):
-    asciiToAlpha = chr(letter)
-    if asciiToAlpha == "e" or asciiToAlpha == "q":
+    if letter == 101 or letter == 113:
         continue
-    print(f"{asciiToAlpha}", end="")
+    print("{}".format(chr(letter)), end="")
