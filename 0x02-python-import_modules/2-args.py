bbce9648b4f1e5bfdcd1c95fbf772ba5bdc409b0
Winful KOfi Taylor <kofi.taylor07@gmail.com>
2023-03-10 16:25:36 +0000

    get command line argument and it length

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
-from sys import argv
-arglen = len(argv)
-if arglen == 1:
-    print("{} arguments.".format(arglen - 1))
-else:
-    print("{} arguments:".format(arglen - 1))
-    for i in range(arglen-1):
-        print("{}: {}".format(i+1, argv[i+1]))
+if __name__ == "__main__":
+    from sys import argv
+    arglen = len(argv)
+    if arglen == 1:
+        print("{} arguments.".format(arglen - 1))
+    elif arglen == 2:
+        print("{} argument:".format(arglen - 1))
+        print("{}: {}".format(arglen-1, argv[arglen-1]))
+    else:
+        print("{} arguments:".format(arglen - 1))
+        for i in range(arglen-1):
+            print("{}: {}".format(i+1, argv[i+1]))
