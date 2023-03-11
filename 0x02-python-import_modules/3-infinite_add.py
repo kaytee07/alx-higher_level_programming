3fb58e666462e2561dac4e040a00fd6a9f55b22a
Winful KOfi Taylor <kofi.taylor07@gmail.com>
2023-03-11 05:00:29 +0000

    infinite add error fixed

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,9 +2,6 @@
 if __name__ == "__main__":
     from sys import argv
     total = 0
-    if (len(argv) == 1):
-        print("{}".format(total))
-    else:
-        for i in argv[1:]:
+    for i in argv[1:]:
             total += int(i)
     print("{}".format(total))
