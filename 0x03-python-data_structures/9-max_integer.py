13f0c89d19ec854b2a7a884f050632aa821c0c8d
Winful KOfi Taylor <kofi.taylor07@gmail.com>
2023-03-12 13:01:38 +0000

    print integers git push

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,10 @@
-#!/bin/usr/python3
+#!/usr/bin/python3
 def max_integer(my_list=[]):
     if len(my_list) == 0:
-        return None
-    largest_number = my_list[0]
-    for i in my_list[1:]:
-        if i > largest_number:
-            largest_number = i
-    return largest_number
+        return "None"
+    else:
+        max = my_list[0]
+        for i in range(len(my_list)):
+            if my_list[i] > max:
+                max = my_list[i]
+        return max
