938a7d574dbde5b12d08e8ee898726144dbffaee
Winful KOfi Taylor <kofi.taylor07@gmail.com>
2023-03-12 08:22:33 +0000

    switch values of variables

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/bin/usr/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    largest_number = my_list[0]
+    for i in my_list:
+        if i > largest_number:
+            largest_number = i
+    return largest_number
