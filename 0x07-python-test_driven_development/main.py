554d2ad0744466fc3dba5e4d1da2c901aad9687d
kaytee07 <kofi.taylor07@gmail.com>
2023-03-31 04:36:56 +0000

    test for the size of an integer

--- 0x07-python-test_driven_development/main.py
+++ 0x07-python-test_driven_development/main.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
-matrix_divided = __import__('2-matrix_divided').matrix_divided
+print_square = __import__('4-print_square').print_square
 
-matrix = [
-        [1, 2, 3],
-        [4, 5, 6]
-    ]
-print(matrix_divided(matrix, 3))
-print(matrix)
+print_square(4)
+print("")
+print_square(10)
+print("")
+print_square(0)
+print("")
+print_square(1)
+print("")
+try:
+    print_square(-1)
+except Exception as e:
+    print(e)
+print("")
