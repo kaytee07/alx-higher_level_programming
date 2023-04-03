7bfefbe0c72a7817905454a369820e9d03da6fc2
kaytee07 <kofi.taylor07@gmail.com>
2023-04-03 16:22:14 +0000

    class methods and static methods

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -34,7 +34,7 @@ class Rectangle:
             raise TypeError("height must be an integer")
         elif value < 0:
             raise ValueError("height must be >= 0")
-        self.__height = value 
+        self.__height = value
 
     def area(self):
         """returns the area of the triangle"""
