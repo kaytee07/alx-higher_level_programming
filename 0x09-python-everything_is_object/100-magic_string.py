25ad2766c7b6919a984d8edfef2f1adfd138bd1e
kaytee07 <kofi.taylor07@gmail.com>
2023-04-04 22:35:04 +0000

    cpython

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""print magic string"""
+def magic_string():
+    """print magic string"""
+    if not hasattr(magic_string, 'counter'):
+        magic_string.counter = 1
+    else:
+        magic_string.counter += 1
+
+    result = "BestSchool" * magic_string.counter
+    return result
---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""locked class"""
+class LockedClass:
+    def __init__(self):
+        self.__locked = False
+
+    def __setattr__(self, attr, value):
+        if self.__locked and attr != "first_name":
+            raise AttributeError("New instance attributes cannot be created")
+        else:
+            self.__dict__[attr] = value
+
+    def __getattr__(self, attr):
+        if attr not in self.__dict__:
+            raise AttributeError(f"'{self.__class__.__name__}' ")
+        return self.__dict__[attr]
+
+    def lock(self):
+        self.__locked = True
+
+    def unlock(self):
+        self.__locked = False   
