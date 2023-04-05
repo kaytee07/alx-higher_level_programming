b8bbda4814936a88b927c1870fc101a5c04f4b65
kaytee07 <kofi.taylor07@gmail.com>
2023-04-05 18:23:13 +0000

    max integer unittesting

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -7,9 +7,6 @@ from max_integer import max_integer
 
 class TestMaxInteger(unittest.TestCase):
 
-    def test_docstring(self):
-        self.assertIsNotNone(max_integer.__doc__)
-
     def test_empty_list(self):
         self.assertEqual(max_integer([]), None)
 
@@ -30,6 +27,27 @@ class TestMaxInteger(unittest.TestCase):
         self.assertEqual(max_integer([1.0, 2.0, 3.0]), 3.0)
         self.assertEqual(max_integer([3.0, 1.0, 2.0]), 3.0)
         self.assertEqual(max_integer([2.0, 3.0, 1.0]), 3.0)
+
+    def test_max_integer_max_at_end(self):
+        self.assertEqual(max_integer([5, 7, 2, 3, 10]), 10)
+
+    def test_max_integer_max_at_beginning(self):
+        self.assertEqual(max_integer([10, 5, 7, 2, 3]), 10)
+
+    def test_max_integer_max_in_middle(self):
+        self.assertEqual(max_integer([5, 7, 10, 2, 3]), 10)
+
+    def test_max_integer_one_negative(self):
+        self.assertEqual(max_integer([-5, 7, 2, 3]), 7)
+
+    def test_max_integer_only_negative(self):
+        self.assertEqual(max_integer([-5, -7, -2, -3]), -2)
+
+    def test_max_integer_one_element(self):
+        self.assertEqual(max_integer([5]), 5)
+
+    def test_max_integer_empty_list(self):
+        self.assertIsNone(max_integer([]))
 
     def test_strings(self):
         self.assertRaises(TypeError, max_integer, ["a", "b", "c"])
