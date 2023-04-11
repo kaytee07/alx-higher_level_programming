ec3d1581f656a1595832ba893b0571414eae8a51
kaytee07 <kofi.taylor07@gmail.com>
2023-04-11 12:54:20 +0000

    check if object is an instance of a class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+return true if object is an instance of class otherwise
+False
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    takes an object and a class and checks if the object 
+    is an instance of the class
+    """
+    return type(obj) == a_class
