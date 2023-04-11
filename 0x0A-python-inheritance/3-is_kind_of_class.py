e1557972903839e9b2b0e39dc3b0d47681d7d441
kaytee07 <kofi.taylor07@gmail.com>
2023-04-11 13:05:56 +0000

    check if obj is an instance of a class or parent class inherit from main class

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,17 @@
+#!usr/bin/python3
+"""
+return true if object is an instance of, or inherited from
+the specified class otherwise false
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    checks if obj is an instance of a_class or obj
+    is an instance of a class that inherited from a_class
+    """
+
+    if type(obj) == a_class or issubclass(type(obj), a_class):
+        return True
+    else:
+        return False
