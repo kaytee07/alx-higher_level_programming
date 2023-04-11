e0a1a74296284c4f9b1c068007362cf6f94db5f1
kaytee07 <kofi.taylor07@gmail.com>
2023-04-11 11:35:33 +0000

    print attribute and methods

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+"""this program print all atribute and methods of an object"""
+
+
+def lookup(obj):
+    """
+    return all the attribute and methods of a class in a list
+    Args:
+        obj: this is the class whose methods and attr are returned
+    """
+    return dir(obj)
