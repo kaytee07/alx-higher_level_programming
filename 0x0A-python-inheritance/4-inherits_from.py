194e316e75d90c2a30b4beb6895edca2f08fb2a8
kaytee07 <kofi.taylor07@gmail.com>
2023-04-11 14:13:35 +0000

    emptry base geormetry

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""check if object is an instance if a class that inherited from a specfied class directly or indirectly"""
+
+
+def inherits_from(obj, a_class):
+    """
+    Return True if obj is an instance
+    of a class that inherited
+    directly or indirectly from a
+    class and False if otherwise
+
+    :param obj: The object to check
+    :param a_class: The class to check against
+    :return: True if obj is an instance of a subclass 
+    of a_class, and False if otherwise
+
+    """
+
+    return a_class in type(obj).__bases__
---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""empty class"""
+
+
+class BaseGeometry:
+    """this is an empty class calle dbase geometry"""
+    pass
