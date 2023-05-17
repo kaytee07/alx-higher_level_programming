a5f27d4e03ec1ad4adfdf6f592d9286ac8f20e8d
kaytee07 <kofi.taylor07@gmail.com>
2023-05-17 14:43:09 +0000

    create new db and user

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,9 @@
+-- create a database 'htbn_0d_2' and 'user_0d_2'
+-- the 'user_0d_2' should have only SELECT privilege in the
+-- database 'hbtn_0d_2'
+-- 'user_0d_2' password should be set to 'user_0d_2_pwd'
+-- script shoyld not fail if user exist
+
+CREATE DATABASE IF NOT EXISTS hbtn_od_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
