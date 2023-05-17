aad037cf73a74a2d7d1dcc5360f8366809bd62e0
kaytee07 <kofi.taylor07@gmail.com>
2023-05-17 19:51:30 +0000

    corrections

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,8 +1,8 @@
 -- create db 'hbtn_0d_usa' and a table 'states' in the db
 
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
-PRIMARY KEY(id)
-id INT     NOT NULL AUTO_INCREMENT,
-name VARCHAR(256)   NOT NULL;
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`states` (
+    PRIMARY KEY(`id`),
+    `id`   INT          NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(256) NOT NULL
 );
