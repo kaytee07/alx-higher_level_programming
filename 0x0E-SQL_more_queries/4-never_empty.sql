071acf9aa14b4a68da955dad516806105654c7b6
kaytee07 <kofi.taylor07@gmail.com>
2023-05-17 19:39:15 +0000

    corrections

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -2,7 +2,7 @@
 -- attribute names id INT with a default value of 1
 -- name VARCHAR(256) script shouldn't fail if table exist
 
-CREATE TABLE IF NOT EXISTS 'id_not_null'(
-'id' INT     DEFAULT 1,
-'name' VARCHAR(256)
+CREATE TABLE IF NOT EXISTS `id_not_null` (
+    `id`   INT          DEFAULT 1,
+    `name` VARCHAR(256)
 );
