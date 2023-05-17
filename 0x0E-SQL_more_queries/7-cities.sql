b9e03a87952cf8aff00998e67ef74ba138bef2db
kaytee07 <kofi.taylor07@gmail.com>
2023-05-17 19:52:40 +0000

    corrections

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,8 +1,7 @@
 -- create db 'hbtn_0d_usa' and table 'cities'
 
-CREATE DATABASE IF NOT EXISTS 'hbtn_0d_usa';
-CREATE TABLE IF NOT EXISTS 'hbtn_0d_usa'.'cities' (
-id INT NOT NULL UNIQUE PRIMARY KEY('id') AUTO_INCREMENT,
-state_id INT NOT NULL, FOREIGN KEY('state_id') REFERENCES 'hbtn_0d_usa.states(id)',
-name VARCHAR(256) NOT NULL;
-);
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities
+       (id INT NOT NULL UNIQUE PRIMARY KEY AUTO_INCREMENT,
+       state_id INT NOT NULL, FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id),
+       name VARCHAR(256) NOT NULL);
