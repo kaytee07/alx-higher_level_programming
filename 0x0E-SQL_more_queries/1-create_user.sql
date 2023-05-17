d91c1c665317fe1aa750c23a983a5cde461a0cdd
kaytee07 <kofi.taylor07@gmail.com>
2023-05-17 18:41:23 +0000

    minor corrections

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -2,5 +2,5 @@
 -- user_0d_1 password should be set to user_0d_1_pwd
 -- if user exist script should not fail
 
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVELEGES ON *.* TO 'user_0d_1'@'localhost'
+CREATE USER IF NOT EXISTS'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -4,6 +4,6 @@
 -- 'user_0d_2' password should be set to 'user_0d_2_pwd'
 -- script shoyld not fail if user exist
 
-CREATE DATABASE IF NOT EXISTS hbtn_od_2;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON 'hbtn_0d_2'.* TO 'user_0d_2'@'localhost';
+GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost'
