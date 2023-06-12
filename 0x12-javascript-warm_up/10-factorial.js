cb0fcdb493e812a378d79d9800b45b22521b9fef
kaytee07 <kofi.taylor07@gmail.com>
2023-06-12 13:53:30 +0000

    print factorial of a number

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+if (process.argv.length <= 2 || !Number(process.argv[2])) {
+  console.log(1);
+} else {
+  let factorial = 1;
+  for (let i = Number(process.argv[2]); i > 0; i--) {
+    factorial *= i;
+  }
+  console.log(factorial);
+}
