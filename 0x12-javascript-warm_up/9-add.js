bb468a28522c9b05d9a9cd1112f458af7fd74c67
kaytee07 <kofi.taylor07@gmail.com>
2023-06-12 13:40:09 +0000

    add argumnet passed on cmd

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+if (process.argv.length <= 3) {
+  console.log('NaN');
+} else {
+  let sum = 0;
+  for (let i = 2; i < process.argv.length; i++) {
+    sum += Number(process.argv[i]);
+  }
+  console.log(sum);
+}
