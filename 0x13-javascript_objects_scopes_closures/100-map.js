8d57ad7f81f901a9dc8bca3e90555e2b368caf6e
kaytee07 <kofi.taylor07@gmail.com>
2023-06-13 17:43:25 +0000

    create new list using map

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
 const list = require('./100-data.js').list;
 console.log(list);
-console.log(list.map((val, index) => val * index));
+let newList = list.map((val, index) => val * index);
+console.log(newList);
