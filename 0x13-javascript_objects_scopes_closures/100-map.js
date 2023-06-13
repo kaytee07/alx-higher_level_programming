38e0a23a28222bf308d9b55bcf48096def7342ce
kaytee07 <kofi.taylor07@gmail.com>
2023-06-13 18:18:20 +0000

    make files executable

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const list = require('./100-data.js').list;
 console.log(list);
-let newList = list.map((val, index) => val * index);
+const newList = list.map((val, index) => val * index);
 console.log(newList);
