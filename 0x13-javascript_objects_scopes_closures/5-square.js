1ece8750878a9a268db63077cd3f36f9539dd3c7
kaytee07 <kofi.taylor07@gmail.com>
2023-06-13 16:21:50 +0000

    correct indentation errors on square

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -2,9 +2,7 @@
 const Rectangle = require('./4-rectangle');
 class Square extends Rectangle {
   constructor (size) {
-    if (size > 0) {
-      super(size, size);
-    }
+    super(size, size);
   }
 }
 module.exports = Square;
