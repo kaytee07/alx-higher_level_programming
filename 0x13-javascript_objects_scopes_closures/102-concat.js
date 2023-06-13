72f1870f62d4b4c6546e99aa9db5d61e2c01a4c7
kaytee07 <kofi.taylor07@gmail.com>
2023-06-13 18:15:01 +0000

    move

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const fs = require('fs');
-const fileC1 = fs.readFileSync(process.argv[2], toString());
-const fileC2 = fs.readFileSync(process.argv[3], toString());
-fs.writeFileSync(process.argv[4], fileC1 + fileC2);
+const fileC1 = fs.readFileSync(process.argv[2], 'utf8');
+const fileC2 = fs.readFileSync(process.argv[3], 'utf8');
+fs.writeFileSync(process.argv[4], fileC1 + '\n' + fileC2 + '\n', 'utf8');
