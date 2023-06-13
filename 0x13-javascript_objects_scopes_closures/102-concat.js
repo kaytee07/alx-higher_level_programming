9ebcf9fbed609e4a3bc630a28b57badf7e8336f3
kaytee07 <kofi.taylor07@gmail.com>
2023-06-13 18:20:13 +0000

    make files executable

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const fs = require('fs');
-const fileC1 = fs.readFileSync(process.argv[2], 'utf8');
-const fileC2 = fs.readFileSync(process.argv[3], 'utf8');
-fs.writeFileSync(process.argv[4], fileC1 + '\n' + fileC2 + '\n', 'utf8');
+const file1 = fs.readFileSync(process.argv[2]).toString();
+const file2 = fs.readFileSync(process.argv[3]).toString();
+fs.writeFileSync(process.argv[4], file1 + file2);
