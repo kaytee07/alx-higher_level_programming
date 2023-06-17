fbec2fa99b5be11552f1b08d259b0a622172916c
kaytee07 <kofi.taylor07@gmail.com>
2023-06-17 13:04:31 +0000

    delete all state with a init

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""print first state object from the database hbtn_0e_6_usa"""
+
+import sys
+from model_state import State, Base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+
+if __name__ == '__main__':
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for instance in session.query(State).filter(State.name.like('%a')):
+        session.delete(instance)
+    session.commit()
