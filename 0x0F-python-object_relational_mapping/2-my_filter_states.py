45408b34a85aa07880babe50cb1c0180daed4a0b
kaytee07 <kofi.taylor07@gmail.com>
2023-06-17 04:51:54 +0000

    filter state

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -4,29 +4,14 @@ import MySQLdb
 import sys
 
 
-def search_states(username, password, database, state_name):
-    """
-    print state that matches the name searched
-    """
-    db = MySQLdb.connect(host='localhost', port=3306,
-                         user=username, passwd=password, db=database)
-
+if __name__ == '__main__':
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cursor = db.cursor()
-    query = f"SELECT * FROM states WHERE name = '{state_name}' ORDER BY id ASC"
-    cursor.execute(query)
+    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
+                   .format(sys.argv[4]))
     rows = cursor.fetchall()
     for row in rows:
         print(row)
     cursor.close()
     db.close()
-
-
-if __name__ == '__main__':
-    if len(sys.argv) == 5:
-        user = sys.argv[1]
-        passwd = sys.argv[2]
-        db = sys.argv[3]
-        state_name = sys.argv[4]
-        search_states(user, passwd, db, state_name)
-    else:
-        print("Usage: enter email password db use")
