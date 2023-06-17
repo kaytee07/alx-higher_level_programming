58098a0156ff8ad76f5b3f3ed7be31e89ac11ff4
kaytee07 <kofi.taylor07@gmail.com>
2023-06-17 14:34:57 +0000

    add

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -2,17 +2,19 @@
 """
 create a class definition of a state and an instance Base
 """
-from sqlalchemy import Column, String, Integer
+from sqlalchemy import Column, Integer, String, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
-
-Base = declarative_base()
+mymetadata = MetaData()
+Base = declarative_base(metadata=mymetadata)
 
 
 class State(Base):
-    """model for state where columns are set"""
+    """
+    Class with id and name attributes of each state
+    """
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
     cities = relationship("City", backref="states")
