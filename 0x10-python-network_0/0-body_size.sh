9da827536147e07c351a4ff55f88b65b87212a5a
kaytee07 <kofi.taylor07@gmail.com>
2023-06-27 16:52:40 +0000

    size

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# a bashscript that displays the size of the body of url passed
+
+
+if [ $# -lt 1 ];
+then
+    echo "Usage: $0 <URL>"
+    exit 1
+fi
+
+url=$1
+response=$(curl -sI $url)
+content_length=$( echo "$response" | grep -i 'Content-Length' | awk '{print $2}')
+echo "$content_length"
