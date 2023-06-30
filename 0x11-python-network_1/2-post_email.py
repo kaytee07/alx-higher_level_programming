e6d5f8e0cf1c94125fa8aeb332b4f0653ceb648f
kaytee07 <kofi.taylor07@gmail.com>
2023-06-30 21:21:01 +0000

    two

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""pass email as POST parameter to url and display body"""
+
+
+import sys
+import urllib.request
+
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+    mail = sys.argv[2]
+    body = {'email': mail}
+    params = urllib.parse.urlencode(body).encode('utf-8')
+    req = urllib.request.Request(url, body, method='POST')
+    with urllib.request.urlopen(req) as response:
+        res = response.read().decode('utf-8')
+    print(res)
