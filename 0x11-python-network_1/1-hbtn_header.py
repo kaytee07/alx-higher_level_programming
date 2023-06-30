8369d64a2d185fa39b16b889a0ac84a3c6f25269
kaytee07 <kofi.taylor07@gmail.com>
2023-06-30 20:33:46 +0000

    header

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -5,8 +5,6 @@
 import sys
 import urllib.request
 
-if not sys.argv[1]:
-    print('<Usage>: Enter URL')
 with urllib.request.urlopen(sys.argv[1]) as response:
     x_request_id = response.getheader("X-Request-Id")
 print(x_request_id)
