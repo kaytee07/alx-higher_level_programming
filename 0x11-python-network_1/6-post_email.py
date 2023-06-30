b9e5ea60081ada5e5922c5bf2994dcf9f7b28677
kaytee07 <kofi.taylor07@gmail.com>
2023-06-30 23:31:55 +0000

    error

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -9,4 +9,4 @@ if __name__ == '__main__':
     email = sys.argv[2]
     data = {'email': email}
     response = requests.post(url, data=data)
-    print('Your email is:', response.text)
+    print(response.text)
