e41030dcc3df0fa928643478f9d56c5cd33bb7f9
kaytee07 <kofi.taylor07@gmail.com>
2023-06-30 21:48:24 +0000

    removed white space

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-python script that takes in a URL, sends a request to the URL 
+python script that takes in a URL, sends a request to the URL
 and displays the body of the response
 """
 
