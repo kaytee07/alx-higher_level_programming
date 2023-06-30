18646b387063739b823ee6eb0b1b117b54eb2fbd
kaytee07 <kofi.taylor07@gmail.com>
2023-06-30 22:28:14 +0000

    add

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" send request and retrieve X-Request-Id"""
+
+import requests
+import sys
+
+if __name__ == '__main__':
+    res = requests.get(sys.argv[1])
+    print(res.headers['X-Request-Id'])
