8a60e2a62bc3d8104f120366c810b8fd73bde249
kaytee07 <kofi.taylor07@gmail.com>
2023-07-25 16:59:41 +0000

    complete task

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -10,24 +10,20 @@ const option = {
 request(option, function (err, res, body) {
   if (err) {
     console.log(err);
-    return;
-  }
-
-  const completed = {};
-  const newbody = JSON.parse(body);
-  for (const index in newbody) {
-    const todo = newbody[index];
-    const user = newbody[index].userId;
-    if (todo.completed) {
-      if (!completed[user]) {
-        completed[user] = 1;
-      } else {
-        completed[user]++;
+  } else if (res.statusCode === 200) {
+    const completed = {};
+    const newbody = JSON.parse(body);
+    for (const index in newbody) {
+      const todo = newbody[index];
+      const user = newbody[index].userId;
+      if (todo.completed) {
+        if (!completed[user]) {
+          completed[user] = 1;
+        } else {
+          completed[user]++;
+        }
       }
-    } else {
-	console.log('An error occured')
     }
+    console.log(completed);
   }
-
-  console.log(completed);
 });
