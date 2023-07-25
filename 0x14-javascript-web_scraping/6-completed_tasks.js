60d468bdc91e78b6fc51510bed873006814e0b4c
kaytee07 <kofi.taylor07@gmail.com>
2023-07-25 16:43:11 +0000

    complete task

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+const timeoutInMilliseconds = 10 * 1000;
+const option = {
+  url,
+  timeout: timeoutInMilliseconds
+};
+
+request(option, function (err, res, body) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  const completed = {};
+  const newbody = JSON.parse(body);
+  for (const index in newbody) {
+    const todo = newbody[index];
+    const user = newbody[index].userId;
+    if (todo.completed) {
+      if (!completed[user]) {
+        completed[user] = 1;
+      } else {
+        completed[user]++;
+      }
+    }
+  }
+
+  console.log(completed);
+});
