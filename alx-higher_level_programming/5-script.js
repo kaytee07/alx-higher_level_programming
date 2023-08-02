53759b1d14b2c0da827497c5bc3b7c9807692d06
kaytee07 <kofi.taylor07@gmail.com>
2023-08-02 16:18:31 +0000

    add list tags on click

---
+++ alx-higher_level_programming/5-script.js
@@ -0,0 +1,7 @@
+/* JavaScript script that adds a <li>
+element to a list when the user clicks on the tag DIV#add_item */
+$(document).ready(function () {
+  $('#add_item').on('click', function () {
+    $('ul.my_list').append('<li>Item</li>');
+  });
+});
